458e78ea63d44ebedd65a209fc2838717e9f790d
Mikael <mikaanastacioteixeira@gmail.com>
2026-04-03 02:36:31 -0300

    add: new endpoint that returns the logged-in user

--- src/main/java/com/mika/tasks/controller/UserController.java
+++ src/main/java/com/mika/tasks/controller/UserController.java
@@ -2,6 +2,7 @@ package com.mika.tasks.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +27,11 @@ public class UserController {
         
         return ResponseEntity.status(HttpStatus.CREATED).body(u);
         
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDTO> getUser(){
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUser());
     }
 }
